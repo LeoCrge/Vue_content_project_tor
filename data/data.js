f7ac667622b899802a3e38bcbfe41fb7048ea50f
LeoCrge <leo.carouge7@gmail.com>
2025-10-28 21:34:51 +0100

    end

--- data/data.js
+++ data/data.js
@@ -3,19 +3,19 @@ window.topics = [
     {
         title: "It was just an accident",
         desc: "Vahid, an Azerbaijani auto mechanic, was once imprisoned by Iranian authorities. During his sentence, he was interrogated blindfolded. One day, a man named Eghbal enters his workshop. His prosthetic leg creaks, and Vahid thinks he recognizes one of his former torturers.",
-        img:"../assets/just_an_accident.png",
+        img:"assets/just_an_accident.png",
         infoUrl: "https://en.wikipedia.org/wiki/It_Was_Just_an_Accident",
         trailer: "https://www.youtube.com/watch?v=nF04v-ze2Yc" },
     {
         title: "Bugonia",
         desc: "Two conspiracy obsessed young men kidnap the high-powered CEO of a major company, convinced that she is an alien intent on destroying planet Earth.",
-        img:"../assets/bugonia.png",
+        img:"assets/bugonia.png",
         infoUrl: "https://en.wikipedia.org/wiki/Bugonia_(film)",
         trailer: "https://www.youtube.com/watch?v=bd_5HcTujfc" },
     {  
         title:"One Battle After Another",
         desc: "When their evil nemesis resurfaces after 16 years, a band of ex-revolutionaries reunite to rescue the daughter of one of their own.",
-        img:"../assets/one_battle.png",
+        img:"assets/one_battle.png",
         infoUrl: "https://en.wikipedia.org/wiki/One_Battle_After_Another",
         trailer: "https://www.youtube.com/watch?v=feOQFKv2Lw4"
 
